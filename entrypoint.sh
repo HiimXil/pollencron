8f7b9b97e2963cbada9a37337d474f394e95d006
Xil <mail@xil.boats>
2023-04-04 10:09:29 +0200

    no timezone

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 printenv | grep -v "no_proxy" >> /etc/environment
-timedatectl set-timezone "$TZ"
 touch /etc/cron.d/pollencron
 echo "$CRON /app/script.sh >> /app/log 2>&1" > /etc/cron.d/pollencron
 chmod 0644 /etc/cron.d/pollencron
