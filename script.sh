36e9288f5ade2bfcdbfce02cd56f144b2566c117
Xil <mail@xil.boats>
2023-04-13 11:17:01 +0200

    -s on curl

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "cron running"
 # Script to run pollen cron job
 res=$(curl -s https://www.pollens.fr/risks/thea/counties/"$DEPARTEMENT")
 # Take pollen Name and his level
@@ -6,8 +7,7 @@ lvlBouleau=$(echo "$res" | jq -r '.risks[] | select(.pollenName == "Bouleau") | 
 lvlavg=$(echo "$res" | jq '[.risks[] | .level ]| add/length')
 # Send message to discord
 if [ "$lvlBouleau" -gt 2 ] || [ "$(echo "$lvlavg>=1"| bc)" -eq 1 ]; then
-    curl  -X POST -H "Content-Type: application/json" -d '{"content":"ALERTE <@'"$DISCORD_ID"'> \nNiveau de pollen pour le Bouleau : '"$lvlBouleau"' \nNiveau moyen de pollen moyen : '"$lvlavg"'"}' "$DISCORD_WEBHOOK"
+    curl  -s -X POST -H "Content-Type: application/json" -d '{"content":"ALERTE <@'"$DISCORD_ID"'> \nNiveau de pollen pour le Bouleau : '"$lvlBouleau"' \nNiveau moyen de pollen moyen : '"$lvlavg"'"}' "$DISCORD_WEBHOOK"
 else
-    curl  -X POST -H "Content-Type: application/json" -d '{"content":"Niveau de pollen pour le Bouleau : '"$lvlBouleau"' \nNiveau moyen de pollen moyen : '"$lvlavg"'"}' "$DISCORD_WEBHOOK"
+    curl  -s -X POST -H "Content-Type: application/json" -d '{"content":"Niveau de pollen pour le Bouleau : '"$lvlBouleau"' \nNiveau moyen de pollen moyen : '"$lvlavg"'"}' "$DISCORD_WEBHOOK"
 fi
-
