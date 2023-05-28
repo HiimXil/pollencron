4602772b01c45efcd91a0eb4a993a6bea0a2f988
Xil <mail@xil.boats>
2023-05-28 14:33:34 +0200

    Add Gramine

--- script.sh
+++ script.sh
@@ -4,10 +4,11 @@ echo "cron running"
 res=$(curl -s https://www.pollens.fr/risks/thea/counties/"$DEPARTEMENT")
 # Take pollen Name and his level
 lvlBouleau=$(echo "$res" | jq -r '.risks[] | select(.pollenName == "Bouleau") | .level')
+lvlGramine=$(echo "$res" | jq -r '.risks[] | select(.pollenName == "Gramin\u00e9es") | .level')
 lvlavg=$(echo "$res" | jq '[.risks[] | .level ]| add/length')
 # Send message to discord
-if [ "$lvlBouleau" -gt 2 ] || [ "$(echo "$lvlavg>=1"| bc)" -eq 1 ]; then
-    curl  -s -X POST -H "Content-Type: application/json" -d '{"content":"ALERTE <@'"$DISCORD_ID"'> \nNiveau de pollen pour le Bouleau : '"$lvlBouleau"' \nNiveau moyen de pollen moyen : '"$lvlavg"'"}' "$DISCORD_WEBHOOK"
+if [ "$lvlBouleau" -gt 2 ] || [ "$lvlGramine" -gt 2 ] || [ "$(echo "$lvlavg>=1"| bc)" -eq 1 ]; then
+    curl  -s -X POST -H "Content-Type: application/json" -d '{"content":"ALERTE <@'"$DISCORD_ID"'> \nNiveau de pollen pour le Bouleau : '"$lvlBouleau"' \nNiveau de pollen pour les Gramine : '"$lvlGramine"' \nNiveau moyen de pollen moyen : '"$lvlavg"'"}' "$DISCORD_WEBHOOK"
 else
-    curl  -s -X POST -H "Content-Type: application/json" -d '{"content":"Niveau de pollen pour le Bouleau : '"$lvlBouleau"' \nNiveau moyen de pollen moyen : '"$lvlavg"'"}' "$DISCORD_WEBHOOK"
+    curl  -s -X POST -H "Content-Type: application/json" -d '{"content":"Niveau de pollen pour le Bouleau : '"$lvlBouleau"' \nNiveau de pollen pour les Gramines : '"$lvlGramine"' \nNiveau moyen de pollen moyen : '"$lvlavg"'"}' "$DISCORD_WEBHOOK"
 fi
